725b41cbf7043abfdd907abee3a6d84c5c024e52
cannnAvar <72388136+cannnAvar@users.noreply.github.com>
2024-11-23 21:27:02 +0300

    Orospu Çocugunu Siktim

--- script/character.gd
+++ script/character.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 
 @export var max_jump = 2
 var jump
+var is_looking_left = true
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,18 +14,30 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and jump > 0:
+	if Input.is_action_just_pressed("Jump") and jump > 0:
 		velocity.y = JUMP_VELOCITY * -1
 		jump -= 1
 	
 	if is_on_floor():
 		jump = max_jump
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	
+	# Get the input direction: -1, 0, 1
+	var direction := Input.get_axis("Left", "Right")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+
+	# When input is left look left. When input is right look right 
+	# Don't fuck with me 
+	if Input.is_action_just_pressed("Left") and is_looking_left == false:
+		scale.x *= -1
+		is_looking_left = true
+	if Input.is_action_just_pressed("Right") and is_looking_left == true:
+		scale.x *= -1
+		is_looking_left = false
 
 	move_and_slide()
